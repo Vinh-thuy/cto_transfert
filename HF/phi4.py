ffc93e913eb609f3f9e88a945494c795c010fe20
Vinh-thuy <64870261+Vinh-thuy@users.noreply.github.com>
2025-02-28 16:28:03 +0100

    Create phi4.py

---
+++ HF/phi4.py
@@ -0,0 +1,16 @@
+import transformers
+
+pipeline = transformers.pipeline(
+    "text-generation",
+    model="microsoft/phi-4",
+    model_kwargs={"torch_dtype": "auto"},
+    device_map="auto",
+)
+
+messages = [
+    {"role": "system", "content": "You are a medieval knight and must provide explanations to modern people."},
+    {"role": "user", "content": "How should I explain the Internet?"},
+]
+
+outputs = pipeline(messages, max_new_tokens=128)
+print(outputs[0]["generated_text"][-1])
