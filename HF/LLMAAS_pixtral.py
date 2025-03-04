e81dfd5065114a64b6812c1a65527e5341345267
Vinh-thuy <64870261+Vinh-thuy@users.noreply.github.com>
2025-03-04 22:27:05 +0100

    Create LLMAAS_pixtral.py

---
+++ HF/LLMAAS_pixtral.py
@@ -0,0 +1,26 @@
+import os 
+from litellm import completion
+
+os.environ["OPENAI_API_KEY"] = "your-api-key"
+
+# openai call
+response = completion(
+    model = "gpt-4-vision-preview", 
+    messages=[
+        {
+            "role": "user",
+            "content": [
+                            {
+                                "type": "text",
+                                "text": "What’s in this image?"
+                            },
+                            {
+                                "type": "image_url",
+                                "image_url": {
+                                "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg"
+                                }
+                            }
+                        ]
+        }
+    ],
+)
