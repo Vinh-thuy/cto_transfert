f1c7eef90978e422319ce05d7d512a93604d24b7
Vinh-thuy <64870261+Vinh-thuy@users.noreply.github.com>
2025-04-02 03:13:20 +0200

    Delete langgraph_stream directory

--- langgraph_stream/agent/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Module agent pour le workflow LangGraph
-"""
-
-from .workflow import execute_graph_stream
-from .nodes import dire_bonjour, dire_bonsoir
-
-__all__ = ["execute_graph_stream", "dire_bonjour", "dire_bonsoir"]
--- langgraph_stream/api/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Module API pour le serveur WebSocket
-"""
-
-from .websocket_server import app, start_server
-
-__all__ = ["app", "start_server"]
